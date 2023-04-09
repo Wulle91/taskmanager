4b06bbef185ca09a91a8f50293f2485ffaf854e1
Wulle91 <>
2023-04-09 17:32:48 +0000

    Add tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    
+    var datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mm, yyyy",
+        i18n: {done:"Select"},  
+    });
+
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+    
   });
